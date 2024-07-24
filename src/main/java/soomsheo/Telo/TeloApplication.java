167027d3390ed0cd48ee73b93e4020fbe30f05da
KimSihyun <rainy02190@gmail.com>
2024-07-24 22:04:50 +0900

    Feat:프레임

--- src/main/java/soomsheo/Telo/TeloApplication.java
+++ src/main/java/soomsheo/Telo/TeloApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TeloApplication {
-
+//test
 	public static void main(String[] args) {
 		SpringApplication.run(TeloApplication.class, args);
 	}
