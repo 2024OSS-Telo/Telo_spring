a0f5dd79cb8a75d010a79dbdd58725bad53fec62
KimSihyun <rainy02190@gmail.com>
2024-07-24 21:55:03 +0900

    Feat:프레임

---
+++ src/main/java/soomsheo/Telo/TeloApplication.java
@@ -0,0 +1,13 @@
+package soomsheo.Telo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/soomsheo/Telo/TeloApplicationTests.java
@@ -0,0 +1,13 @@
+package soomsheo.Telo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
